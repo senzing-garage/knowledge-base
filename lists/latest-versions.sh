7c0101fa0d725bf9666139d1ccb031afbd5d1a36
Michael Dockter <michael@dockter.com>
2021-02-22 15:52:24 -0500

    Deprecated.

--- lists/latest-versions.sh
+++ lists/latest-versions.sh
@@ -1,4 +1,7 @@
-# Deprecated.  Will be removed after June 30, 2021
+#!/usr/bin/env bash
 
-# Contents have moved to
-# https://github.com/Senzing/knowledge-base/blob/master/lists/versions-latest.sh
+echo "ERROR:"
+echo "Deprecated.  Will be removed after June 30, 2021"
+echo "Contents have moved to https://github.com/Senzing/knowledge-base/blob/master/lists/versions-latest.sh"
+
+exit 1
