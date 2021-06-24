30c7c39244116b42e42273a9f9db931139066749
michael@dockter.com <michael@dockter.com>
2021-06-24 14:25:09 -0400

    Update file name

--- lists/latest-versions.sh
+++ lists/latest-versions.sh
@@ -2,6 +2,6 @@
 
 echo "ERROR:"
 echo "Deprecated.  Will be removed after June 30, 2021"
-echo "Contents have moved to https://github.com/Senzing/knowledge-base/blob/master/lists/versions-latest.sh"
+echo "Contents have moved to https://github.com/Senzing/knowledge-base/blob/master/lists/docker-versions-latest.sh"
 
 exit 1
