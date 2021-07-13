d1c656bde3cbea3fc0764da43cbbf6c126c1dcc3
michael@dockter.com <michael@dockter.com>
2021-07-13 10:14:23 -0400

    Version update

--- lists/latest-versions.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "ERROR:"
-echo "Deprecated.  Will be removed after June 30, 2021"
-echo "Contents have moved to https://github.com/Senzing/knowledge-base/blob/master/lists/docker-versions-latest.sh"
-
-exit 1
