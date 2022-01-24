afad15102f670213a7d485107c6485ac39589683
michael@dockter.com <michael@dockter.com>
2022-01-24 09:45:30 -0500

    Update date

--- lists/docker-versions-latest.sh
+++ lists/docker-versions-latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Generated on 2022-01-19 by https://github.com/Senzing/dockerhub-util dockerhub-util.py version: 1.0.3 update: 2022-01-19
+# Generated on 2022-01-24 by https://github.com/Senzing/dockerhub-util dockerhub-util.py version: 1.0.3 update: 2022-01-19
 
 export SENZING_DOCKER_IMAGE_VERSION_ADMINER=1.0.1
 export SENZING_DOCKER_IMAGE_VERSION_APT=1.0.6
