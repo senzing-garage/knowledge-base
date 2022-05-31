24ad04c74391ac95a5e7177257445cd2526a3e83
docktermj <michael@dockter.com>
2022-05-31 10:11:23 -0400

    Update versions

--- lists/docker-versions-latest.sh
+++ lists/docker-versions-latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Generated on 2022-05-26 by https://github.com/Senzing/dockerhub-util dockerhub-util.py version: 1.0.5 update: 2022-05-23
+# Generated on 2022-05-31 by https://github.com/Senzing/dockerhub-util dockerhub-util.py version: 1.0.5 update: 2022-05-23
 
 export SENZING_DOCKER_IMAGE_VERSION_ADMINER=1.0.1
 export SENZING_DOCKER_IMAGE_VERSION_APT=1.0.8
