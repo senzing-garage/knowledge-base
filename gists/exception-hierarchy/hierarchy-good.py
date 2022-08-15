9ee224459206527aef0accc59154766c1a454866
docktermj <michael@dockter.com>
2022-08-15 15:29:37 -0400

    Example of Exception hierarchy

---
+++ gists/exception-hierarchy/hierarchy-good.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python3
+
+
+class G2Exception(Exception):
+    '''Base exception for G2 related python code'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        print("here - 0 ")
+
+    def __str__(self):
+        result = []
+        for arg in self.args:
+            message = arg
+            if isinstance(arg, Exception):
+                message = "{0}:".format(arg.__class__.__name__)
+            if message not in result:
+                result.append(message)
+        return " ".join(result)
+
+
+class G2L1Exception(G2Exception):
+    '''DocString'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        print("here - 1")
+
+
+class G2L2Exception(G2L1Exception):
+    '''DocString'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        print("here - 2")
+
+
+class G2L1xException(G2Exception):
+    '''DocString'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        print("here - this shouldn't print")
+
+
+class G2L3Exception(G2L2Exception):
+    '''DocString'''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        print("here - 3")
+
+# -----------------------------------------------------------------------------
+# Utility functions
+# -----------------------------------------------------------------------------
+
+
+def blow_up():
+    raise G2L3Exception("blowing up")
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+
+    try:
+        print("testing...")
+        blow_up()
+    except G2Exception as err:
+        print(err)
